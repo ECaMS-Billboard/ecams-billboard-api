6cca1d2741713f39a94e4ee448852298003f6aac
AirlineExpress <samueljswedo@lewisu.edu>
2025-04-10 07:04:01 +1200

    Authentication 3

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ const User = db.model(
 // Authentication code
 // Passport configuration
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "/auth/google/callback"
 },
 (accessToken, refreshToken, profile, done) => {
